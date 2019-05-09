c32c1913214a5f582fac198f19f468044a55792c
Jessica Poe <jesspoe00@gmail.com>
2019-05-09 13:49:16 -0700

    added validation for password to be present

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   has_secure_password
   validates :email, uniqueness: { case_sensitive: false }
+  validates :password, presence: true
 
   has_many :user_groups
   has_many :groups, through: :user_groups
